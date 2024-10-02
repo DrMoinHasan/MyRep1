b0ec9ee0619d244bcb61c364a4ede60df51802e1
Moin Hasan <111106063+DrMoinHasan@users.noreply.github.com>
2024-10-02 13:03:25 +0530

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   const myValue = process.env.MY_VARIABLE;
-  console.log('Value of MY_VARIABLE:', Hello);
-  res.send('This is my response: ${Hello}');
+  console.log('Value of MY_VARIABLE:', myValue)
+  res.send('This is my response: ${myValue}');
 });
 
 app.listen(3000);
